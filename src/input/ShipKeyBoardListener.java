08dd29137ea29fcc2b832719619a187433aec65e
Rick Lubbers <ik@ricklubbers.nl>
2015-03-21 18:03:16 +0100

    Added rolling, fixed mergeconflicts

--- src/input/ShipKeyBoardListener.java
+++ src/input/ShipKeyBoardListener.java
@@ -18,23 +18,18 @@ public class ShipKeyBoardListener implements ActionListener{
    private Ship ship;
    private boolean tiltForward;
    private boolean tiltBackward;
+   private boolean rollLeft;
+   private boolean rollRight;
 
    
    public ShipKeyBoardListener(Main app){
        
        app.getInputManager().addMapping("tiltForward", new KeyTrigger(KeyInput.KEY_W));
        app.getInputManager().addMapping("tiltBackward", new KeyTrigger(KeyInput.KEY_S));
-<<<<<<< HEAD
-       app.getInputManager().addMapping("tiltLeft", new KeyTrigger(KeyInput.KEY_A));
-       app.getInputManager().addMapping("tiltRight", new KeyTrigger(KeyInput.KEY_D));
-       app.getInputManager().addMapping("shoot", new KeyTrigger(KeyInput.KEY_SPACE));
-       app.getInputManager().addListener(this, new String[]{"tiltForward", "tiltBackward", "shoot", "tiltLeft", "tiltRight"});
-=======
        app.getInputManager().addMapping("rollLeft", new KeyTrigger(KeyInput.KEY_A));
        app.getInputManager().addMapping("rollRight", new KeyTrigger(KeyInput.KEY_D));
        app.getInputManager().addMapping("shoot", new KeyTrigger(KeyInput.KEY_SPACE));
        app.getInputManager().addListener(this, new String[]{"tiltForward", "tiltBackward", "rollLeft", "rollRight", "shoot"});
->>>>>>> 3d9745d1c076433b11d2033af45c1c94c7a155c0
    }
     
     public void setShip(Ship ship){
@@ -42,37 +37,27 @@ public class ShipKeyBoardListener implements ActionListener{
     }
     public void onAction(String name, boolean isPressed, float tpf) {
         if (isPressed){
-<<<<<<< HEAD
             if (name.equals("shoot")){
-=======
-            if(name.equals("tiltForward")){
-                this.ship.wPressed(); 
-            } else if (name.equals("tiltBackward")){
-                this.ship.sPressed();
-            } else if (name.equals("rollLeft")) {
-                this.ship.aPressed();
-            } else if (name.equals("rollRight")) {
-                this.ship.dPressed();
-            } else if (name.equals("shoot")){
->>>>>>> 3d9745d1c076433b11d2033af45c1c94c7a155c0
                 this.ship.shoot();
             }
         }
         
         if (name.equals("tiltForward")){
-                this.tiltForward = isPressed;
-            } else if (name.equals("tiltBackward")){
-                this.tiltBackward = isPressed;
-            }
+            this.tiltForward = isPressed;
+        } else if (name.equals("tiltBackward")){
+            this.tiltBackward = isPressed;
+        } else if (name.equals("rollLeft")){
+            this.rollLeft = isPressed;
+        } else if (name.equals("rollRight")){
+            this.rollRight = isPressed;
+        }
     }
     
     public void step(){
-        if (tiltForward){            
-            this.ship.wPressed(); 
-        }
-        if (tiltBackward) {
-            this.ship.sPressed();
-        }
+        if (tiltForward) this.ship.wPressed(); 
+        if (tiltBackward) this.ship.sPressed();
+        if (rollLeft) this.ship.sPressed();
+        if (rollRight) this.ship.sPressed();
     }
     
 }

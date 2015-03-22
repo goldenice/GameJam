052db88df677c4a9774d5a587cd8db0dd1998f6d
Gerryflap <gerry2211@gmail.com>
2015-03-22 08:51:49 +0100

    added Networkmanager (Might already be in place) and fixed the position of the player/ship

---
+++ src/mygame/NetworkManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Gerryflap
+ */
+public class NetworkManager {
+    private Socket sock;
+    private String address;
+    private int port;
+    
+    
+    public NetworkManager(){
+        
+    }
+    
+}

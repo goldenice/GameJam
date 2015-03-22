6cd5011bb0b3e7006ef9d5537b25dccae0bfe8ea
Rick Lubbers <ik@ricklubbers.nl>
2015-03-22 09:22:08 +0100

    Nieuwe networkmanager

--- src/mygame/NetworkManager.java
+++ src/mygame/NetworkManager.java
@@ -4,20 +4,45 @@
  */
 package mygame;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 /**
  *
- * @author Gerryflap
+ * @author Rick Lubbers
  */
-public class NetworkManager {
-    private Socket sock;
-    private String address;
-    private int port;
+public class NetworkManager implements Runnable {
+
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
     
+    public NetworkManager(Socket socket) {
+        try {
+            this.socket = socket;
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (Exception e) {
+            System.err.println("CHAOS AND DESTRUCTION WHILE USING SOCKET!");
+        }
+    }
     
-    public NetworkManager(){
+    public void run() {
+        try {
+            while (true) {
+                String input = null;
+                if ((input = in.readLine()) != null) {
+                    // Parse this stuff!
+
+                    input = null;
+                }
+            }
+        } catch (Exception e) {
         
+        }
     }
     
 }
